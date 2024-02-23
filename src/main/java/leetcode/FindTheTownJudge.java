1ff459dbe93b2254ea870ff97ac52d6da9b0d043
malvann <45212444+malvann@users.noreply.github.com>
2024-02-23 11:24:50 +0100

    wip

--- src/main/java/leetcode/FindTheTownJudge.java
+++ src/main/java/leetcode/FindTheTownJudge.java
@@ -1,28 +1,23 @@
 package leetcode;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FindTheTownJudge {
-    public int findJudge(int n, int[][] trusts) {
-        if (n == 1 && trusts.length == 0) return 1;
+    public int findJudge(int n, int[][] trust) {
+        if (n == 1 && trust.length == 0) return 1;
 
-        Map<Integer, Integer> posTrustCountMap = new HashMap<>();
-        Set<Integer> trustedPos = new HashSet<>();
-        for (int[] trust : trusts) {
-            posTrustCountMap.computeIfPresent(trust[1], (k, v) -> ++v);
-
-            if (posTrustCountMap.containsKey(trust[0])) posTrustCountMap.remove(trust[0]);
-            else if (trustedPos.contains(trust[1])) posTrustCountMap.remove(trust[1]);
-            else posTrustCountMap.putIfAbsent(trust[1], 1);
-
-            trustedPos.add(trust[0]);
-        }
-        return posTrustCountMap.entrySet().stream().filter(e -> e.getValue() == n - 1)
+        Map<Integer, List<int[]>> collect = Stream.of(trust).parallel()
+                .collect(Collectors.groupingBy(arr -> arr[1]));
+        Optional<Map.Entry<Integer, List<int[]>>> first = collect.entrySet().stream().parallel()
+                .filter(e -> e.getValue().size() == n - 1)
+                .findFirst();
+        return first.filter(integerListEntry -> collect.values().stream()
+                        .parallel()
+                        .flatMap(Collection::stream)
+                        .noneMatch(num -> Objects.equals(num[0], integerListEntry.getKey())))
                 .map(Map.Entry::getKey)
-                .findFirst()
                 .orElse(-1);
     }
 }
--- src/test/java/leetcode/FindTheTownJudgeTest.java
+++ src/test/java/leetcode/FindTheTownJudgeTest.java
@@ -17,6 +17,7 @@ class FindTheTownJudgeTest {
 
     private static Stream<Arguments> test() {
         return Stream.of(
+                Arguments.of(6, new int[][]{{1, 2}, {6, 4}, {3, 2}, {2, 6}, {4, 5}, {6, 1}, {1, 4}, {1, 5}, {2, 3}, {2, 1}, {4, 3}, {4, 2}, {2, 5}, {4, 1}, {2, 4}, {6, 5}, {3, 5}}, 5),
                 Arguments.of(4, new int[][]{{1, 3}, {1, 4}, {2, 3}, {2, 4}, {4, 3}}, 3),
                 Arguments.of(2, new int[][]{{1, 2}}, 2),
                 Arguments.of(3, new int[][]{{1, 3}, {2, 3}}, 3),
