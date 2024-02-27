400f8484905b20a9a54c983bac185be8ed417167
malvann <45212444+malvann@users.noreply.github.com>
2024-02-27 15:09:38 +0100

    wip

--- src/main/java/leetcode/CheapestFlightsWithinKStops.java
+++ src/main/java/leetcode/CheapestFlightsWithinKStops.java
@@ -5,18 +5,17 @@ import java.util.stream.IntStream;
 
 public class CheapestFlightsWithinKStops {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int[] trip = IntStream.generate(() -> Integer.MAX_VALUE).limit(n).toArray();
-        trip[src] = 0;
+        int[] flCosts = IntStream.generate(() -> Integer.MAX_VALUE).limit(n).toArray();
+        flCosts[src] = 0;
 
         while (k-- > -1) {
-            int[] temp = Arrays.copyOf(trip, n);
+            int[] temp = Arrays.copyOf(flCosts, n);
             for (int[] flight : flights) {
-                if (trip[flight[0]] == Integer.MAX_VALUE) continue;
-                temp[flight[1]] = Math.min(temp[flight[1]], trip[flight[0]] + flight[2]);
+                if (flCosts[flight[0]] == Integer.MAX_VALUE) continue;
+                temp[flight[1]] = Math.min(temp[flight[1]], flight[2] + flCosts[flight[0]]);
             }
-            trip = temp;
+            flCosts = temp;
         }
-
-        return trip[dst] == Integer.MAX_VALUE ? -1 : trip[dst];
+        return flCosts[dst] == Integer.MAX_VALUE ? -1 : flCosts[dst];
     }
 }
