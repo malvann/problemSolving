3cb18d89f1173dc468edf1a3131aa654cd0dcc9e
malvann <45212444+malvann@users.noreply.github.com>
2024-03-24 10:24:03 +0100

    wip

--- src/main/java/leetcode/FindTheDuplicateNumber.java
+++ src/main/java/leetcode/FindTheDuplicateNumber.java
@@ -5,6 +5,15 @@ import java.util.Set;
 
 public class FindTheDuplicateNumber {
     public int findDuplicate(int[] nums) {
+        int[] matrix = new int[100000];
+        for (int n : nums) {
+            if (matrix[n - 1] != 0) return n;
+            matrix[n - 1] = 1;
+        }
+        return 0;
+    }
+
+    public int findDuplicate1(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for (int n : nums) {
             int size = set.size();
@@ -12,20 +21,5 @@ public class FindTheDuplicateNumber {
             if (size == set.size()) return n;
         }
         return 0;
-    }
-
-    public int findDuplicate1(int[] nums) {
-        for (int i = 1; i <= nums.length; i++) {
-            if (i == nums[i - 1]) continue;
-
-            int buf;
-            while (nums[i - 1] != i) {
-                if (nums[i - 1] == nums[nums[i - 1] - 1]) return nums[i - 1];
-                buf = nums[nums[i - 1] - 1];
-                nums[nums[i - 1] - 1] = nums[i - 1];
-                nums[i - 1] = buf;
-            }
-        }
-        return -1;
     }
 }
